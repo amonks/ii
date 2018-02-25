5c50ca0d5538531ad70c6fcd6222d0ee9aca739a
Andrew Monks <andrew.monks@samsara.com>
2018-02-24 18:14:14 -0800

    fixup convert-zones.fish

--- convert-zones.fish
+++ convert-zones.fish
@@ -1,9 +1,9 @@
 cd zones
 for file in *
 	if test -s $file
-		../bzfttr53rdutil -domain $file -zone-file $file > ../modules/converted-zones/$file.tf
+		../bzfttr53rdutil -domain $file -zone-file $file > ../terraform/converted-zones/$file.tf
 	else
-		if test -s ../modules/sources/$file.tf
+		if test -s ../terraform/sources/$file.tf
 			echo "deleting empty with source $file"
 			# rm $file
 		else
