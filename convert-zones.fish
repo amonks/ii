493b3c81ec3442fb567530695b4d1f22d52d1fa9
monks <andrew.monks@samsara.com>
2019-06-07 13:50:18 -0700

    changes

--- convert-zones.fish
+++ convert-zones.fish
@@ -1,17 +1,12 @@
-cd zones
-for file in *
+if test -d terraform/converted-zones
+	rm -r terraform/converted-zones
+end
+mkdir -p terraform/converted-zones
+
+for file in zones/*
 	if test -s $file
-		../tfz53 -domain $file -zone-file $file > ../terraform/converted-zones/$file.tf
-	else
-		if test -s ../terraform/sources/$file.tf
-			echo "deleting empty with source $file"
-			# rm $file
-		else
-			echo "deleting empty without source $file"
-			# rm $file
-		end
+		./tfz53 -domain (basename $file) -zone-file $file > terraform/converted-zones/(basename $file).tf
 	end
 end
 
-cd ..
 terraform fmt
---
+++ terraform/converted-zones/popefucker.com.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_zone" "popefucker-com" {
+   name = "popefucker.com"
+}
+
+resource "aws_route53_record" "popefucker-com-A" {
+   zone_id = "${aws_route53_zone.popefucker-com.zone_id}"
+   name = "popefucker.com."
+   type = "A"
+   ttl = "10800"
+   records = ["159.203.95.137"]
+}
