404d6b02364c2cd74842b4c9a0f985f972699759
Andrew Monks <a@monks.co>
2020-01-17 23:39:09 -0800

    cross platform

--- convert-zones.fish
+++ convert-zones.fish
@@ -1,3 +1,10 @@
+if test (uname -s) = "Darwin"
+	set tfz53 ./tfz53-macos
+end
+if test (uname -s) = "Linux"
+	set tfz53 ./tfz53-linux
+end
+
 if test -d terraform/converted-zones
 	rm -r terraform/converted-zones
 end
@@ -5,7 +12,7 @@ mkdir -p terraform/converted-zones
 
 for file in zones/*
 	if test -s $file
-		./tfz53 -domain (basename $file) -zone-file $file > terraform/converted-zones/(basename $file).tf
+		$tfz53 -domain (basename $file) -zone-file $file > terraform/converted-zones/(basename $file).tf
 	end
 end
 
