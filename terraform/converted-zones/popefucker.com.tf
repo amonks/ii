35cc9e8463658b107155279d4f500c9830c52541
monks <andrew.monks@samsara.com>
2020-02-20 22:53:40 -0800

    popefucker *

--- terraform/converted-zones/popefucker.com.tf
+++ terraform/converted-zones/popefucker.com.tf
@@ -9,3 +9,11 @@ resource "aws_route53_record" "popefucker-com-A" {
   ttl     = "300"
   records = ["159.203.95.137"]
 }
+
+resource "aws_route53_record" "wildcard-popefucker-com-A" {
+  zone_id = aws_route53_zone.popefucker-com.zone_id
+  name    = "*.popefucker.com."
+  type    = "A"
+  ttl     = "300"
+  records = ["159.203.95.137"]
+}
