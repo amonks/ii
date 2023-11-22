c63f775d73a7c784679b13c6dde88c4dfe4a83e2
monks <a@monks.co>
2023-11-22 02:41:55 -0600

    homepage: periodically re-query watches; update db

--- apps/homepage/main.go
+++ apps/homepage/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 
 	"github.com/a-h/templ"
 	"monks.co/apps/movies/db"
@@ -34,17 +37,21 @@ func run() error {
 	}
 	defer db.Stop()
 
-	watches, err := db.AllWatches()
-	if err != nil {
-		return err
-	}
-
-	data := &PageData{
-		Watches: watches,
-	}
+	fetcher := periodically(db.AllWatches)
+	defer fetcher.stop()
 
 	mux := http.NewServeMux()
-	mux.Handle("/", templ.Handler(Homepage(data)))
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		v, err := fetcher.get()
+		if err != nil {
+			serve.InternalServerError(w, req, err)
+			return
+		}
+		h := templ.Handler(Homepage(&PageData{
+			Watches: v,
+		}))
+		h.ServeHTTP(w, req)
+	})
 
 	ctx := sigctx.New()
 	addr := fmt.Sprintf("127.0.0.1:%d", *port)
@@ -53,4 +60,57 @@ func run() error {
 	}
 
 	return nil
+}
+
+type periodic[T any] struct {
+	stopped bool
+	mu      sync.Mutex
+
+	v   T
+	err error
+}
+
+var errUnset = fmt.Errorf("unset")
+
+func periodically[T any](f func() (T, error)) *periodic[T] {
+	p := &periodic[T]{
+		err: errUnset,
+	}
+	go func() {
+		for {
+			log.Println("reload")
+			if p.isStopped() {
+				return
+			}
+			val, err := f()
+			p.set(val, err)
+
+			time.Sleep(30 * time.Second)
+		}
+	}()
+	return p
+}
+
+func (p *periodic[T]) isStopped() bool {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	return p.stopped
+}
+
+func (p *periodic[T]) stop() {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.stopped = true
+}
+
+func (p *periodic[T]) get() (T, error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	return p.v, p.err
+}
+
+func (p *periodic[T]) set(v T, err error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.v, p.err = v, err
 }
