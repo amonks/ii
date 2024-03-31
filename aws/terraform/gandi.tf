3a94cf0aa9dc46c6036fc56db2672289ac13fc92
monks <a@monks.co>
2024-03-31 00:45:21 -0500

    add piano.computer

--- aws/terraform/gandi.tf
+++ aws/terraform/gandi.tf
@@ -43,6 +43,11 @@ resource "gandi_nameservers" "gandi_domain_needsyourhelp_org" {
   nameservers = aws_route53_zone.needsyourhelp-org.name_servers
 }
 
+resource "gandi_nameservers" "gandi_domain_piano_computer" {
+  domain      = "piano.computer"
+  nameservers = aws_route53_zone.piano-computer.name_servers
+}
+
 resource "gandi_nameservers" "gandi_domain_popefucker_com" {
   domain      = "popefucker.com"
   nameservers = aws_route53_zone.popefucker-com.name_servers
