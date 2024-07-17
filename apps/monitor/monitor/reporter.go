1aaea953210d24613af80461487203fdebcdeb04
monks <a@monks.co>
2024-07-17 16:40:48 -0500

    fix bugs; run taskmaker

--- apps/monitor/monitor/reporter.go
+++ apps/monitor/monitor/reporter.go
@@ -3,6 +3,7 @@ package monitor
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
@@ -10,6 +11,9 @@ import (
 type Reporter map[string]Monitor
 
 func (rep Reporter) Run(ctx context.Context, dur time.Duration) error {
+	if err := rep.Report(); err != nil {
+		return err
+	}
 	tick := time.NewTicker(dur)
 	for {
 		select {
@@ -26,16 +30,19 @@ func (rep Reporter) Run(ctx context.Context, dur time.Duration) error {
 func (rep Reporter) Report() error {
 	for snitch, mon := range rep {
 		if err := mon.Check(); err == nil {
+			log.Printf("success: '%s'", snitch)
 			if err := rep.snitch(snitch); err != nil {
 				return fmt.Errorf("error snitching to '%s': %w", snitch, err)
 			}
+		} else {
+			log.Printf("fail: '%s': %s", snitch, err)
 		}
 	}
 	return nil
 }
 
 func (rep Reporter) snitch(snitch string) error {
-	if _, err := http.Get("https://nosnitch.in/" + snitch); err != nil {
+	if _, err := http.Get("https://nosnch.in/" + snitch); err != nil {
 		return err
 	}
 	return nil
