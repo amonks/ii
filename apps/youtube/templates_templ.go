d6ccda10cf915267ca0c3d651b84bb1776905a23
monks <a@monks.co>
2025-01-10 16:32:20 -0600

    rm conflicting templs from dev servers

--- apps/youtube/templates_templ.go
+++ apps/youtube/templates_templ.go
@@ -72,7 +72,7 @@ func Index(history []*model.HistoryEntry) templ.Component {
 					var templ_7745c5c3_Var4 string
 					templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(entry.Time)
 					if templ_7745c5c3_Err != nil {
-						return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates.templ`, Line: 15, Col: 23}
+						return templ.Error{Err: templ_7745c5c3_Err, FileName: `apps/youtube/templates.templ`, Line: 15, Col: 23}
 					}
 					_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 					if templ_7745c5c3_Err != nil {
@@ -85,7 +85,7 @@ func Index(history []*model.HistoryEntry) templ.Component {
 					var templ_7745c5c3_Var5 string
 					templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(entry.Title)
 					if templ_7745c5c3_Err != nil {
-						return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates.templ`, Line: 16, Col: 20}
+						return templ.Error{Err: templ_7745c5c3_Err, FileName: `apps/youtube/templates.templ`, Line: 16, Col: 20}
 					}
 					_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 					if templ_7745c5c3_Err != nil {
@@ -103,7 +103,7 @@ func Index(history []*model.HistoryEntry) templ.Component {
 					var templ_7745c5c3_Var6 string
 					templ_7745c5c3_Var6, templ_7745c5c3_Err = templ.JoinStringErrs(entry.Time)
 					if templ_7745c5c3_Err != nil {
-						return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates.templ`, Line: 19, Col: 22}
+						return templ.Error{Err: templ_7745c5c3_Err, FileName: `apps/youtube/templates.templ`, Line: 19, Col: 22}
 					}
 					_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var6))
 					if templ_7745c5c3_Err != nil {
@@ -116,7 +116,7 @@ func Index(history []*model.HistoryEntry) templ.Component {
 					var templ_7745c5c3_Var7 string
 					templ_7745c5c3_Var7, templ_7745c5c3_Err = templ.JoinStringErrs(entry.Title)
 					if templ_7745c5c3_Err != nil {
-						return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates.templ`, Line: 20, Col: 19}
+						return templ.Error{Err: templ_7745c5c3_Err, FileName: `apps/youtube/templates.templ`, Line: 20, Col: 19}
 					}
 					_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var7))
 					if templ_7745c5c3_Err != nil {
